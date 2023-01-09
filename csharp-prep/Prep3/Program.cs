9095aa95e4847853048b9cf81c8e9dfceb8b526a
allenjandrew <allenjandrew@gmail.com>
2023-01-09 12:32:36 -0700

    prep 3 guess a random number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 101);
+
+        int guess;
+
+        do
+        {
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magic)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else if (guess < magic)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (guess == magic)
+            {
+                Console.WriteLine("You got it!");
+            }    
+        } while (guess != magic);
+            
+            
+
     }
 }
