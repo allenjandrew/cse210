d08d5baea3d2a5ba39c206e5a99d6c4daa5e27c8
allenjandrew <allenjandrew@gmail.com>
2023-02-03 21:11:44 -0700

    added notes to Program.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,6 +1,12 @@
 using System;
 using Develop02.Classes;
 
+/* I have exceeded requirements by: adding a ton of prompts to my prompt generator; allowing the user to write
+    with or without a prompt; storing the filename in a variable so the user only has to save the file (without
+    entering a new filename every time); providing a save as ... option; providing an open file ... option;
+    allowing the user to add new global notes to their journal; allowing the user to add a new prompt; etc.
+*/
+
 namespace Develop02
 {
     public class Program
